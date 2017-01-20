74fd90bb9f6551600ee209352ad5c0151186ae51
jba0040 <jba0040@alu.ubu.es>
2017-01-20 21:14:14 +0100

    Mejora en el movimiento del empujón

--- Project Lucio/ProjectSettings/GetWaved.cs
+++ Project Lucio/ProjectSettings/GetWaved.cs
@@ -17,13 +17,13 @@ public class GetWaved : MonoBehaviour {
 	void OnCollisionEnter(Collision c)
 	{
 		// force is how forcefully we will push the player away from the enemy.
-		float force = 5;
+		float force = 8;
 
 		// If the object we hit is the enemy
 		if (c.gameObject.tag == "Wave")
 		{
 			// Calculate Angle Between the collision point and the player
-			Vector3 dir = c.contacts[0].point - transform.position;
+			Vector3 dir = c.gameObject.transform.position - transform.position;
 			// We then get the opposite (-Vector3) and normalize it
 			dir = -dir.normalized;
 			// And finally we add force in the direction of dir and multiply it by force. 
