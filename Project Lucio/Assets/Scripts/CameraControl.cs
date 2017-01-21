4b55d57f1ca3570601c0dcbd000430e01ecda499
mrr0088 <mrr0088@alu.ubu.es>
2017-01-21 13:43:51 +0100

    Control de Cámara y Prefab Ataque Especial

---
+++ Project Lucio/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+	//Time between the camera would start to move and the time the camera actually starts to move (to make it smooth)
+    public float m_DampTime = 0.1f;                 
+	//Kind of a padding to prevent the tanks to touch the limits of the camera
+    public float m_ScreenEdgeBuffer = 2f;           
+	//Minimun size of the camera
+    public float m_MinSize = 6.5f;                  
+	//Number of targets the camera has to cover
+    public Transform[] m_Targets; 
+
+
+    private Camera m_Camera;
+	//How fast it zooms
+    private float m_ZoomSpeed;                      
+	//Desired position: in the middle of all the tanks
+    private Vector3 m_DesiredPosition;              
+
+
+    private void Awake()
+    {
+        m_Camera = gameObject.GetComponent<Camera>();
+    }
+
+	//So that it moves in the same function that the tanks move
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+
+    private void Move()
+    {
+        FindAveragePosition();
+		Vector3 velocity = Vector3.zero;
+		Vector3 desiredPos = new Vector3 (m_DesiredPosition.x, m_DesiredPosition.y, transform.position.z);
+		//first arg: current position. Second arg: Where do we want to go. Third arg: current velocity. Fourth arg: how much time do we want it to take
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, m_DampTime);
+    }
+
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+			//For instance, if a tank is dead, we do not want it to be taken into consideration
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0) {
+			averagePos /= numTargets;
+		}
+
+		m_DesiredPosition = averagePos;
+    }
+
+
+    private void Zoom()
+    {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+
+    private float FindRequiredSize()
+    {
+		//To get the desired position depending on the rig of the camera
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+			//For instance, if a target is dead, we do not want it to be taken into consideration
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+			//Whether the target is going away by the horizontal or vertical direction, we take the max of both
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+        }
+        
+		//Extra distance so that it makes sure the target is into the zoom
+        size += m_ScreenEdgeBuffer;
+
+		//To prevent zooming too much, we have the min size
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+	//Setting the default configuration
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
---
+++ Project Lucio/Assets/Scripts/waveGenerator.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waveGenerator : MonoBehaviour {
+
+	public GameObject sphere; 
+	public GameObject killerSphere;
+	public Collider[] colliders;
+	public ParticleSystem explosion;
+	// Use this for initialization
+
+
+	void Start () {
+		sphere.transform.position = transform.position;
+		sphere.transform.localScale = transform.localScale;
+		sphere.tag = "Player";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		sphere.SetActive (true);
+		float targetScale = 5f;
+		float shrinkSpeed = 3.5f;
+		float growSpeed = 2.5f;
+		Vector3 scale = transform.localScale;
+		sphere.tag = "Player";
+		if (Input.GetKey (KeyCode.Space)) {
+
+			sphere.transform.localScale = Vector3.Lerp (sphere.transform.localScale, 
+			             new Vector3 (transform.localScale.x + targetScale,
+			             transform.localScale.y + targetScale,
+			             transform.localScale.z + targetScale), Time.deltaTime * growSpeed);
+			scale = sphere.transform.localScale;
+			//			sphereCollider.transform.localScale = Vector3.Lerp (sphere.transform.localScale, 
+			//			                                                    new Vector3 (transform.localScale.x + targetScale,
+			//			            transform.localScale.y + targetScale,
+			//			            transform.localScale.z + targetScale), Time.deltaTime * growSpeed);
+		} else {
+			//			sphere.transform.localScale = Vector3.Lerp (sphere.transform.localScale, 
+			//			                                            new Vector3 (1f, 1f, 1f), Time.deltaTime * shrinkSpeed);
+			//			sphereCollider.transform.localScale = Vector3.Lerp (sphere.transform.localScale, 
+			//			                                                    new Vector3 (1f, 1f, 1f), Time.deltaTime * shrinkSpeed);
+
+			
+		}
+		
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			sphere.SetActive(false);
+			explosion.transform.position = transform.position;
+			explosion.Play();
+			killerSphere.SetActive(true);
+			killerSphere.transform.position = transform.position;
+			killerSphere.transform.localScale = Vector3.Lerp (sphere.transform.localScale, new Vector3 (targetScale,targetScale,targetScale), Time.deltaTime * growSpeed);
+
+			killerSphere.GetComponent<SphereCollider>().transform.localScale = killerSphere.transform.localScale;
+			colliders = Physics.OverlapSphere (killerSphere.transform.position, targetScale);
+
+			foreach(Collider col in colliders)
+			{
+				if(col.tag == "Enemy")
+					col.gameObject.SetActive(false);
+			}
+			killerSphere.SetActive(false);
+			sphere.transform.localScale = transform.localScale;
+			killerSphere.transform.localScale = sphere.transform.localScale;
+		}
+
+	}
+
+	void FixedUpdate () {
+
+	}
+
+
+}
