108951ce850149f90c4db8f805d47a56d38f2b32
jba0040 <jba0040@alu.ubu.es>
2017-01-21 23:32:09 +0100

    Muerte por pincho Script

---
+++ Project Lucio/Assets/Scripts/DeadBySpike.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadBySpike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.SetActive (false);
+		}
+	}
+}
