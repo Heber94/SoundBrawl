f602acefc63f8200b84816bbc1ac0b2870dc63ed
mrr0088 <mrr0088@alu.ubu.es>
2017-01-21 14:34:17 +0100

    Simple Scripts
    Script para matar al jugador si se sale del plano
    Script para matar al jugador si colisiona con un pincho

---
+++ Project Lucio/Assets/Scripts/DeadByLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadByLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnCollisionEnter(Collision col)
+	{
+	
+		if (col.gameObject.tag == "Player") 
+		{
+			col.gameObject.SetActive(false);
+		}
+	}
+}
