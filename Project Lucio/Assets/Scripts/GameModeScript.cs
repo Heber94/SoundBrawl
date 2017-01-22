f1b9345fff65ad730bcceee07737e49bb4756063
mrr0088 <mrr0088@alu.ubu.es>
2017-01-22 14:27:16 +0100

    Main Menu & Spawn

---
+++ Project Lucio/Assets/Scripts/GameModeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameModeScript : MonoBehaviour {
+    public static int nJugadores;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(transform.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void chooseMode(int nPlayers)
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
