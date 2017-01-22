8735b08418eeb897f00128468d08019c0d94177e
mrr0088 <mrr0088@alu.ubu.es>
2017-01-22 12:02:50 +0100

    Main Menu

---
+++ Project Lucio/Assets/Scripts/GameModeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameModeScript : MonoBehaviour {
+
+    public static int nJugadores;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void chooseMode(int nPlayers)
+    {
+        nJugadores = nPlayers;
+        SceneManager.LoadScene("Level0");
+    }
+}
