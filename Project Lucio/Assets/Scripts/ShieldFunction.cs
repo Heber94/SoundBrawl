b41df964c19963c18fc25a654228ce5c421d369a
victorcas04 <victorcas95@gmail.com>
2017-03-01 10:27:24 +0100

    Shield Scripts
    Vuelvo a subir los scripts del escudo.
    Para que funcione el generator hay que crear un slider y asignársele.

---
+++ Project Lucio/Assets/Scripts/ShieldFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldFunction : MonoBehaviour {
+
+    public string prefix;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = GetComponentInParent<Transform>().position;
+        transform.localPosition = GetComponentInParent<Transform>().localPosition;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Debug.Log("collision");
+        //Debug.Log(prefix);
+        //If our shield touches any collider, we decide...
+        //foreach (Collider col in colliders)
+        //{
+        //If a player try to come close to us, it's bounced (just a little, not like the waves)
+        //if (col.gameObject.tag == "Player" && col.transform.position != gameObject.GetComponentInParent<Transform>().position)
+        //{
+        //    col.gameObject.GetComponent<Rigidbody>().AddForce(-(gameObject.transform.position - col.transform.position), ForceMode.Impulse);
+        //    Debug.Log("player");
+        //}
+        //If our shield touch a wave, that one is destroyed
+        if (col.gameObject.tag == "Wave")
+        {
+            Destroy(col.gameObject);
+            Debug.Log("wave");
+        }else if (col.gameObject.tag == "Shield" && col.gameObject.name != (prefix.ToString() + "Shield"))
+        {
+            col.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            Debug.Log("shield");
+        }
+        //}
+    }
+}
---
+++ Project Lucio/Assets/Scripts/shieldGenerator.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class shieldGenerator : MonoBehaviour {
+    AudioSource audiocarga;
+    public AudioClip carga;
+    public AudioClip explota;
+    
+	public GameObject shield; 
+	[HideInInspector] public Collider[] colliders;
+	public ParticleSystem explosion;
+    public string prefix;
+    public float shieldTime = 2f;
+    [HideInInspector] public static float sT;
+    public float timeTillUseAgain = 5f;
+    private float tTUA;
+
+    public Slider shieldSlider;
+    public Color MaxShieldColor = Color.green;
+    public Color MinShieldColor = Color.red;
+
+    void Start () {
+        audiocarga = GetComponent<AudioSource>();
+
+        shield.layer = 8;
+        shield.SetActive(false);
+        sT = shieldTime;
+        tTUA = 0;
+    }
+	
+	void Update () {
+        
+		float targetScale = 5f;
+		float growSpeed = 2.5f;
+		Vector3 scale = transform.localScale;
+
+        //If we're pressing the shield button, it increase each frame till reach maximun size
+        if (tTUA <= 0)
+        {
+            if (Input.GetButtonDown(prefix + "Shield"))
+            {
+                shield.SetActive(true);
+
+                shield.transform.position = transform.position;
+                shield.transform.localScale = transform.localScale / 4;
+            }else if (Input.GetButton(prefix + "Shield"))
+            {
+                audiocarga.clip = carga;
+                audiocarga.loop = false;
+                audiocarga.Play();
+
+                shield.transform.localScale = Vector3.Lerp(shield.transform.localScale,
+                             new Vector3(transform.localScale.x + targetScale,
+                             transform.localScale.y + targetScale,
+                             transform.localScale.z + targetScale), Time.deltaTime * growSpeed);
+                scale = shield.transform.localScale;
+                sT -= Time.deltaTime;
+                shieldSlider.value = ((shieldTime - sT) * 100) / shieldTime;
+            } else if (Input.GetButtonUp(prefix + "Shield"))   
+                //When we release the shield button, it dissapear, play the sound and is restored to its default values
+            {
+                audiocarga.clip = explota;
+                audiocarga.Stop();
+
+                explosion.transform.position = transform.position;
+                explosion.Play();
+
+                shield.SetActive(false);
+            }
+            else if(sT < shieldTime)
+            {
+                sT += Time.deltaTime;
+                shieldSlider.value = ((shieldTime - sT) * 100) / shieldTime;
+            }
+
+            if (sT <= 0)
+            {
+                audiocarga.clip = explota;
+                audiocarga.Stop();
+
+                explosion.transform.position = transform.position;
+                explosion.Play();
+
+                //Debug.Log("shieldExplote!");
+
+                tTUA = timeTillUseAgain;
+                sT = shieldTime;
+
+                shield.SetActive(false);
+            }
+            //shieldSlider.colors = Color.Lerp(MinShieldColor, MaxShieldColor, (float)sT / shieldTime);
+            shieldSlider.GetComponentInChildren<UnityEngine.UI.Image>().color = Color.Lerp(MinShieldColor, MaxShieldColor, (float)sT / shieldTime);
+        }
+        else
+        {
+            tTUA -= Time.deltaTime;
+            shieldSlider.value = (tTUA / timeTillUseAgain) * 100;
+            shieldSlider.GetComponentInChildren<UnityEngine.UI.Image>().color = MinShieldColor;
+        }
+        
+    }
+
+}
+
+
+
